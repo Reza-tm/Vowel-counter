730e8e4e66ab9743c0cbd6274ad45d1c1b7352bf
Reza <rezatm331@gmail.com>
2021-08-27 05:55:55 +0430

    now app can detect UpperCase Vowels

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let textInput = document.getElementById("input");
 let result = document.getElementById("result");
 function vowelsFinder() {
-	let inputValue = textInput.value;
+	let inputValue = textInput.value.toLowerCase();
 	let totalVowels = 0;
 	for (let i = 0; i < inputValue.length; i++) {
 		let letter = inputValue[i];
