f166ac2d593bf5b84a20efe891064a435b8f8afb
Reza <rezatm331@gmail.com>
2021-08-27 05:45:05 +0430

    finish project

---
+++ New folder/script.js
@@ -0,0 +1,14 @@
+let input = document.getElementById("input");
+
+// function vowesFinder() {
+// let totalVolwes = 0;
+// for (let i = 0; i < text.length; i++) {
+// 	let letter = text[i];
+// 	if (letter.match(/([a,o,u,e,i])/)) {
+// 		totalVolwes++;
+// 	}
+// }
+// console.log(totalVolwes);
+// }
+
+// vowesFinder("hello you are my friend");
---
+++ script.js
@@ -0,0 +1,13 @@
+let textInput = document.getElementById("input");
+let result = document.getElementById("result");
+function vowelsFinder() {
+	let inputValue = textInput.value;
+	let totalVowels = 0;
+	for (let i = 0; i < inputValue.length; i++) {
+		let letter = inputValue[i];
+		if (letter.match(/([a,o,u,e,i])/)) {
+			totalVowels++;
+		}
+	}
+	result.textContent = `Vowels number = ${totalVowels}`;
+}
